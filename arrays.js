9f971b5a4ef497ed16e533776c0cc04ac23c6ca3
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-08 17:19:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,3 @@
-let chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"];
 let addElementToBeginningOfArray = () => {}
 let destructivelyAddElementToBeginningOfArray = () => {}
