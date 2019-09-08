86eb4cd2f8ab911d0617212fe1972e2eb892dd58
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-08 17:37:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,6 @@ var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"];
 var addElementToBeginningOfArray = function(array, element) {
  return [...element, array];
 }
-var destructivelyAddElementToBeginningOfArray = () => {}
+var destructivelyAddElementToBeginningOfArray = (array, element) => {
+  array.push(element);
+}
