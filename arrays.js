c61d74807a0c28afa5c39d12a5bd56534464b4c1
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-08 17:39:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,5 @@ var addElementToBeginningOfArray = function(array, element) {
  return [...element, array];
 }
 var destructivelyAddElementToBeginningOfArray = function(array, element) {
-return  array.shift(element);
+return  array.unshift(element);
 }
