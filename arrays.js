6fe15cdab655e23d6772014d625cec2788d00b1f
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-08 18:39:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,33 +1,25 @@
 var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"];
-var addElementToBeginningOfArray = (array, element) => {
- return [element, ...array];
-}
+var addElementToBeginningOfArray = (array, element) => { return [element, ...array];}
 var destructivelyAddElementToBeginningOfArray = function(array, element){
    array.unshift(element);
    return array;
 }
 
-var addElementToEndOfArray = (array, element) => {
-  return [...array, element];
-}
+var addElementToEndOfArray = (array, element) => {  return [...array, element];}
 
 var destructivelyAddElementToEndOfArray = (array, element) =>{
   array.push(element);
   return array;
 }
 
-var accessElementInArray = (array,index) => {
-  return array[index];
-}
+var accessElementInArray = (array,index) => {return array[index];}
 
 var destructivelyRemoveElementFromBeginningOfArray = function (array){
   array.shift();
   return array;
 }
 
-var removeElementFromBeginningOfArray = function(array){
-return  array.slice(1);
-}
+var removeElementFromBeginningOfArray = array => {return  array.slice(1);}
 
 var destructivelyRemoveElementFromEndOfArray = function(array){
   array.pop();
